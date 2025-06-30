dd05d7b38ff12ea936ed6a0a6b1ee059458c56d8
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-06-29 21:27:06 -0700

    Add hello Azure function implementation in index.ts

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import {
+  app,
+  HttpRequest,
+  HttpResponseInit,
+  InvocationContext,
+} from "@azure/functions";
+
+async function hello(
+  request: HttpRequest,
+  context: InvocationContext
+): Promise<HttpResponseInit> {
+  context.log(
+    "Http function was triggered.",
+  );
+
+  return {
+    body: "Hello, world!",
+  };
+}
+
+app.http(
+  "hello",
+  {
+    methods: [
+      "GET",
+      "POST",
+    ],
+    handler: helloWorld1,
+  },
+);
